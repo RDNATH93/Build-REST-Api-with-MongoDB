c5522908968d66f3196ad3ce21bc424e22a61823
RDNATH93 <im_rahuldebnath@outlook.com>
2021-11-23 04:44:40 +0000

    Initialize Project

---
+++ spring-rest-with-mongo/src/main/java/com/learnspring/springrestwithmongo/SpringRestWithMongoApplication.java
@@ -0,0 +1,13 @@
+package com.learnspring.springrestwithmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWithMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestWithMongoApplication.class, args);
+	}
+
+}
---
+++ spring-rest-with-mongo/src/test/java/com/learnspring/springrestwithmongo/SpringRestWithMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspring.springrestwithmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestWithMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
